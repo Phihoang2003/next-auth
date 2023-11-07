bcdc23304cb2059b9edab018a6d08f979971c3dd
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-07 08:18:51 +0700

    initial next-api

---
+++ nextjs-auth/src/app/api/user/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req:Request) {
+
+    try {
+        const body=await req.json();
+        return NextResponse.json(body);
+    } catch (error) {
+        
+    }
+    
+}
